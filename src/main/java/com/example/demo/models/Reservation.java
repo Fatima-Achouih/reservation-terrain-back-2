e4f9b02985cfe7141dfbb0e93b1e0e98e06606aa
Achouih Fatima <achwihfatima@gmail.com>
2023-01-17 13:21:14 +0100

    reservation model

--- src/main/java/com/example/demo/models/Reservation.java
+++ src/main/java/com/example/demo/models/Reservation.java
@@ -1,6 +1,6 @@
 package com.example.demo.models;
 
-import java.util.Date;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,15 +15,39 @@ public class Reservation {
 	@Id
 	@GeneratedValue
 	private long id;
-	
+	private String nom;
+	private String email;
+	private String tel;
 	private String title;
 	private String start;
 	private String end;
+	private String date;
+	private String hi;
+	private String hf;
+	
 	public long getId() {
 		return id;
 	}
 	public void setId(long id) {
 		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
 	}
 	public String getTitle() {
 		return title;
@@ -43,6 +67,25 @@ public class Reservation {
 	public void setEnd(String end) {
 		this.end = end;
 	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getHi() {
+		return hi;
+	}
+	public void setHi(String hi) {
+		this.hi = hi;
+	}
+	public String getHf() {
+		return hf;
+	}
+	public void setHf(String hf) {
+		this.hf = hf;
+	}
+	
 	
 	
 	
